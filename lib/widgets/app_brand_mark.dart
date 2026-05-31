b723e92da932540eba8809c175c2360b5829d526
3000y <821923589@qq.com>
2026-05-31 14:22:01 +0800

    Refine web loading shell motion

--- lib/widgets/app_brand_mark.dart
+++ lib/widgets/app_brand_mark.dart
@@ -41,7 +41,7 @@ class AppBrandMark extends StatelessWidget {
         Text(appBrandName, maxLines: 1, overflow: TextOverflow.ellipsis, style: titleStyle),
         const SizedBox(height: 3),
         Text(
-          'Local Manager for Remote Volumes',
+          'Cloud Volume',
           maxLines: 1,
           overflow: TextOverflow.ellipsis,
           style: subtitleStyle,
